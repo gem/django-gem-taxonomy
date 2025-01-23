ce6c7ec233d35ae47daee8b522866586a8ca5899
Matteo Nastasi <nastasi@openquake.org>
2025-01-23 18:07:18 +0100

    improve server/run.sh commodity

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 . ./venv/bin/activate
-PYTHONPATH=. django-admin runserver --settings server.settings  0.0.0.0:8000
+if [ $# -gt 0 ]; then
+    cmd="$1"
+    shift
+    PYTHONPATH=. django-admin "$cmd" --settings server.settings $@
+else
+    PYTHONPATH=. django-admin runserver --settings server.settings  0.0.0.0:8000
+fi
