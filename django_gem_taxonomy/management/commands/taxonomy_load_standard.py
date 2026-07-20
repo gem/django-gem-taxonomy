50b4e9c1ade3160eb90815a407f22fc90b9a6453
Matteo Nastasi <nastasi@openquake.org>
2026-07-20 16:36:33 +0200

    add '-d|--development' optional argument for 'taxonomy_load_standard' django custom command to reduce verbosity in normal installations

--- django_gem_taxonomy/management/commands/taxonomy_load_standard.py
+++ django_gem_taxonomy/management/commands/taxonomy_load_standard.py
@@ -19,12 +19,12 @@
 
 # import subprocess
 # from django.conf import settings
-from django.core.management import call_command
-from pprint import pprint
-from django.core.management.base import BaseCommand
+import os
+import sys
 import copy
 import json
-import os
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
 from random import randint
 
 from django_gem_taxonomy.models import Attribute, AtomsGroup, Atom, Param
@@ -41,17 +41,14 @@ class Command(BaseCommand):
         parser.add_argument(
             '-n', '--no-dump', help='avoid create json files from DB'
             , action='store_true', default=False)
-        # parser.add_argument(
-        #     '-l', '--list', action='store_true', help='Show projects that need'
-        #     ' maintenance')
-        # parser.add_argument(
-        #     '-f', '--force', action='store_true', help='Rebuild cache in any'
-        #     ' case')
-        # parser.add_argument(
-        #     '-k', '--keep-cache', action='store_true',
-        #     help='Keep the existing cache')
+        parser.add_argument(
+            '-d', '--development', help='enable pdb on exceptions and increase verbosity'
+            , action='store_true', default=False)
 
     def handle(self, *args, **options):
+        if options['development']:
+            from pprint import pprint
+
         tax_json_in = None
         with open(options['json_filename'], 'r') as f:
             tax_json_in = json.load(f)
@@ -62,7 +59,8 @@ class Command(BaseCommand):
                 prog=attr_in['prog'],
                 title=attr_in['title'],
             )
-            pprint(attr)
+            if options['development']:
+                pprint(attr)
 
         for atg_in in tax_json_in['AtomsGroup']:
             atoms_group = AtomsGroup.objects.create(
@@ -71,7 +69,8 @@ class Command(BaseCommand):
                 title=atg_in['title'],
                 attr=Attribute.objects.get(name=atg_in['group'])
             )
-            pprint(atoms_group)
+            if options['development']:
+                pprint(atoms_group)
 
         atom = Atom.objects.create(
             name='_ARG',
@@ -95,11 +94,14 @@ class Command(BaseCommand):
             try:
                 atom_params = (json.loads(at_in['params'])
                                if at_in['params'] else None)
-            except Exception as inst:
+            except Exception:
                 exc_num = 1
-                import pdb ; pdb.set_trace()
+                if options['development']:
+                    import pdb ; pdb.set_trace()
+                sys.exit(exc_num)
 
-            print(atom_name)
+            if options['development']:
+                print(atom_name)
             atom = Atom.objects.create(
                 name=at_in['name'],
                 prog=at_in['prog'],
@@ -115,9 +117,12 @@ class Command(BaseCommand):
                 if atom.name in tax_json_in['AtomsDeps']:
                     for dep in tax_json_in['AtomsDeps'][atom.name]:
                         atom.deps.add(Atom.objects.get(name=dep))
-            except Exception as inst:
+            except Exception:
                 exc_num = 2
-                import pdb ; pdb.set_trace()
+                if options['development']:
+                    import pdb ; pdb.set_trace()
+                sys.exit(exc_num)
+
 
             try:
                 if atom.name in tax_json_in['AtomsDeny']:
@@ -125,7 +130,9 @@ class Command(BaseCommand):
                         atom.deny.add(Atom.objects.get(name=den))
             except Exception as inst:
                 exc_num = 3
-                import pdb ; pdb.set_trace()
+                if options['development']:
+                    import pdb ; pdb.set_trace()
+                sys.exit(exc_num)
 
 
         for param_atom, pa_ins in tax_json_in['Param'].items():
