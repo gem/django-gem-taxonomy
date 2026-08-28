3b7b8d88fd8a1202da813d37cfa9c1f58904e1d4
Matteo Nastasi <nastasi@openquake.org>
2026-08-28 11:25:18 +0200

    managed properly nested commands into tests [4]

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -7,10 +7,18 @@ from django.test import TestCase
 class CommandsTestCase(TestCase):
     def test_taxonomy_load_standard(self):
         "Test taxonomy_load_standard command."
+        
+        data = {'vers_id': '3.3',
+                'vers_desc': 'Version 3.3 year 2024',
+                'is_default': True,
+                'fname': 'tests/data/taxonomy3.3_standard.json'}
 
-        args = ['django_gem_taxonomy_postinstall']
-        opts = {}
+        call_args = ['taxonomy_load_standard', data['vers_id'], data['vers_desc'], '--no-dump']
+        if data['is_default']:
+            call_args.append('--is-default')
+        call_args.append(data['fname'])
+        call_opts = {}
         v_file = StringIO()
         stdout_backup, sys.stdout = sys.stdout, v_file
-        call_command(*args, **opts)
+        call_command(*call_args, **call_opts)
         sys.stdout = stdout_backup
