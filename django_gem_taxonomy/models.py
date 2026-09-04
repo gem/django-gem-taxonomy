af05ea9fed937b06c349e0021cc37279e1b9dff5
Matteo Nastasi <nastasi@openquake.org>
2026-09-04 07:14:15 +0200

    source headers refactoring

--- django_gem_taxonomy/models.py
+++ django_gem_taxonomy/models.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 #
-# oq-geoviewer
-# Copyright (C) 2024 GEM Foundation
+# Copyright (C) 2025-2026 GEM Foundation
 #
-# oq-geoviewer is free software: you can redistribute it and/or modify
+# django-gem-taxonomy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# oq-geoviewer is distributed in the hope that it will be useful,
+# django-gem-taxonomy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
