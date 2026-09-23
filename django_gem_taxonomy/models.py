f3941c3dbf36c6d731e25ca572e96e471eb7a0ed
Matteo Nastasi <nastasi@openquake.org>
2026-09-23 16:22:19 +0200

    final step of multi-stage migration

--- django_gem_taxonomy/models.py
+++ django_gem_taxonomy/models.py
@@ -19,73 +19,13 @@
 from django.db import models
 import json
 
-class Attribute(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    prog = models.IntegerField()
-    title = models.TextField()
-
-
-class AtomsGroup(models.Model):
-    class Meta:
-        unique_together = [['attr', 'prog']]
-
-    name = models.CharField(max_length=256, primary_key=True)
-    prog = models.IntegerField()
-    title = models.TextField()
-    attr = models.ForeignKey(Attribute, on_delete=models.CASCADE)
-    # mutex identify if it is possible or not dropdown multi-selection
-    mutex = models.BooleanField(default=True)
-
-# TODO: parameters description atom(param1[,param2[...,paramN]])
-# class AtomParam(models.Model):
-#     atom = models.ForeignKey(Atom)
-
-# TODO: arguments description atom[:arg1[:arg2[...:argN]]]
-# class AtomArg(models.Model):
-#     atom = models.ForeignKey(Atom)
-
-
-class Atom(models.Model):
-    class Meta:
-        unique_together = [['group', 'prog']]
-
-    name = models.CharField(max_length=32, primary_key=True)
-    prog = models.IntegerField()
-    title = models.TextField()
-    desc = models.TextField()
-    group = models.ForeignKey(AtomsGroup, on_delete=models.CASCADE, null=True)
-    attr = models.ForeignKey(Attribute, on_delete=models.CASCADE, null=True)
-    type = models.TextField()
-    args = models.JSONField(blank=True, null=True)
-    params = models.JSONField(blank=True, null=True)
-    deps = models.ManyToManyField('self', symmetrical=False,
-                                  related_name='revdeps')
-    deny = models.ManyToManyField('self', symmetrical=False,
-                                  related_name='revdeny')
-    # is_pseudoid = models.BooleanField()
-
-    def entry_type(self):
-        return json.loads(self.type)
-
-
-class Param(models.Model):
-    class Meta:
-        unique_together = [['atom', 'name'], ['atom', 'name', 'prog']]
-
-    atom = models.ForeignKey(Atom, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=32)
-    title = models.TextField()
-    desc = models.TextField()
-    prog = models.IntegerField()
-
-
-class Version2(models.Model):
+class Version(models.Model):
     vers = models.CharField(max_length=16, primary_key=True)
     desc = models.TextField()
     is_default = models.BooleanField(default=False)
 
-class Attribute2(models.Model):
-    vers = models.ForeignKey(Version2, on_delete=models.CASCADE)
+class Attribute(models.Model):
+    vers = models.ForeignKey(Version, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     prog = models.IntegerField()
     title = models.TextField()
@@ -94,9 +34,9 @@ class Attribute2(models.Model):
         unique_together = [['vers', 'name'],
                            ['vers', 'prog']]
 
-class AtomsGroup2(models.Model):
-    vers = models.ForeignKey(Version2, on_delete=models.CASCADE)
-    attr = models.ForeignKey(Attribute2, on_delete=models.CASCADE)
+class AtomsGroup(models.Model):
+    vers = models.ForeignKey(Version, on_delete=models.CASCADE)
+    attr = models.ForeignKey(Attribute, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     prog = models.IntegerField()
     title = models.TextField()
@@ -117,10 +57,10 @@ class AtomsGroup2(models.Model):
 #     atom = models.ForeignKey(Atom)
 
 
-class Atom2(models.Model):
-    vers = models.ForeignKey(Version2, on_delete=models.CASCADE)
-    attr = models.ForeignKey(Attribute2, on_delete=models.CASCADE, null=True)
-    group = models.ForeignKey(AtomsGroup2, on_delete=models.CASCADE, null=True)
+class Atom(models.Model):
+    vers = models.ForeignKey(Version, on_delete=models.CASCADE)
+    attr = models.ForeignKey(Attribute, on_delete=models.CASCADE, null=True)
+    group = models.ForeignKey(AtomsGroup, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=32)
     prog = models.IntegerField()
     title = models.TextField()
@@ -142,9 +82,9 @@ class Atom2(models.Model):
         return json.loads(self.type)
 
 
-class Param2(models.Model):
-    vers = models.ForeignKey(Version2, on_delete=models.CASCADE)
-    atom = models.ForeignKey(Atom2, on_delete=models.CASCADE, null=True)
+class Param(models.Model):
+    vers = models.ForeignKey(Version, on_delete=models.CASCADE)
+    atom = models.ForeignKey(Atom, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=32)
     prog = models.IntegerField()
     title = models.TextField()
